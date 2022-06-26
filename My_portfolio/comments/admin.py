272dddce8acfa19025fdc490741b1717ede7c1b4
DavidN04 <ndm04082007@gmail.com>
2022-06-26 17:05:24 +0600

    Admin and edits for the project

--- My_portfolio/comments/admin.py
+++ My_portfolio/comments/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Comment
 
+class CommentAdmin(admin.ModelAdmin):
+	fields = ["user_name", "project", "text"]
+	list_filter = ["user_name", "project"]
+	search_fields = ["text"]
+
+admin.site.register(Comment, CommentAdmin)
 # Register your models here.
--- My_portfolio/mainapp/admin.py
+++ My_portfolio/mainapp/admin.py
@@ -1,2 +1,25 @@
 from django.contrib import admin
+from .models import Project, Image
+
+class ImageInline(admin.StackedInline):
+	model = Image
+	extra = 2
+
+class ProjectAdmin(admin.ModelAdmin):
+	model = Project
+	list_display = ["name", "slug", "url_repository", "description", "status"]
+	fields = ["name", "slug", "url_repository", "description", "status"]
+	list_display_links = ["name", "slug"]
+	list_editable = ["status"]
+	list_filter = ["created", "name", "slug", "status"]
+	search_fields = ["name", "slug", "description"]
+
+	inlines = [ImageInline]
+
+class ImageAdmin(admin.ModelAdmin):
+	fields = ["name", "project", "image"]
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Image, ImageAdmin)
+
 # Register your models here.
